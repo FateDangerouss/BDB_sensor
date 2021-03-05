96ddf3c199b9ee846a33dd3c4152a595889b5929
Sherry <chenji15sss@gmail.com>
2021-03-05 20:22:45 +0800

    20:22 03/05/2020

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import ultrasonic as us
+import threading as th
 from time import sleep
 import camera
 import imageclassify
@@ -6,28 +7,34 @@ import gpio
 
 man_info = 0
 
-out1 = gpio.myGPIO([27,"OUT"])
-out1.output(27,"HIGH")
+vcc = gpio.myGPIO([21, "OUT"])
+vcc.output(21, "HIGH")
 
-mandet = us.Ultrasonic(21,18)
+mandet = us.Ultrasonic(20,26)
 cam = camera.camera()
 img = imageclassify.ImageClassify()
 
-while True:
+def det():
     while True:
-        if mandet.distance() <= 50:
-            man_info = 1
-            print("start working")
-            break
-        else:
-            man_info = 0
-        sleep(0.5)
-    cam.get()
-    img.get_file('object.jpg')
-    item = img.get_item()
-    print(str(item['result']) + '\n')
-    result = ''
-    for i in range(0,5):
-        if item['result'][i]['score'] >= 0.4:
-            result = result + item['result'][i]['root'] + '\n' + item['result'][i]['keyword'] + '\n\n'
-    print(result)
+        while True:
+            if mandet.distance() <= 50:
+                man_info = 1
+                print("start working")
+                break
+            else:
+                man_info = 0
+            sleep(0.5)
+        cam.get()
+        img.get_file('object.jpg')
+        item = img.get_item()
+        print(str(item['result']) + '\n')
+        result = ''
+        for i in range(0,5):
+            if item['result'][i]['score'] >= 0.4:
+                result = result + item['result'][i]['root'] + '\n' + item['result'][i]['keyword'] + '\n\n'
+        print(result)
+
+th_det = th.Thread(target=det(), name='Threading Detective')
+
+while True:
+    th_det.start()
