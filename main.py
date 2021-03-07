3b5ed02dc08cfb26c57febb45def068784ab57fd
Sherry <chenji15sss@gmail.com>
2021-03-07 13:36:26 +0800

    13:36 03/07/2020

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import camera
 import imageclassify
 import gpio
 
-man_info = 0
+state = 0
 
 vcc = gpio.myGPIO([21, "OUT"])
 vcc.output(21, "HIGH")
@@ -16,23 +16,27 @@ img = imageclassify.ImageClassify()
 
 def det():
     while True:
-        while True:
-            if mandet.distance() <= 50:
-                man_info = 1
-                print("start working")
-                break
-            else:
-                man_info = 0
-            sleep(0.5)
-        cam.get()
-        img.get_file('object.jpg')
-        item = img.get_item()
-        print(str(item['result']) + '\n')
-        result = ''
-        for i in range(0,5):
-            if item['result'][i]['score'] >= 0.4:
-                result = result + item['result'][i]['root'] + '\n' + item['result'][i]['keyword'] + '\n\n'
-        print(result)
+        if state == 0:
+            while True:
+                if mandet.distance() <= 50:
+                    state = 1
+                    break
+                else:
+                    sleep(0.5)
+            cam.get()                                                                                                                                                       
+            img.get_file('object.jpg')
+            item = img.get_item()   
+            print(str(item['result']) + '\n')
+            result = ''
+            for i in range(0,5):
+                if item['result'][i]['score'] >= 0.4:
+                    result = result + item['result'][i]['root'] + '\n' + item['result'][i]['keyword'] + '\n\n'
+            print(result)
+            state = 0
+        else:
+            time.sleep(0.5)
+            
+            
 
 th_det = th.Thread(target=det(), name='Threading Detective')
 
